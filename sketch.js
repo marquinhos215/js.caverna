e250dfa6c6748cb02cb9c6ba60f4d3530c7339e1
marquinhos215 <00001113229937sp@al.educacao.sp.gov.br>
2024-05-23 09:37:34 -0300

    adicionando arquivos js e css

--- sketch.js
+++ sketch.js
@@ -1,52 +1,14 @@
-let cor;
-let circuloX;// x
-let circuloY; // y
-
-
-
 function setup() {
-  random(0, 255);
-  createCanvas(400, 400); //width x height
-  background(color(100, 0, 0));
-  cor = color(random(0, 255), random(0, 255), random(0, 255));
-  circuloX =[0,0,0];
-  circuloY=[random(height),random(height),random(height)];
-  
-  
-  
+  createCanvas(600, 600);
+   background("white");
 }
-
-//circuloX= 0,0,0
-//circuloY= 300,150,50
 
 function draw() {
   
-  fill(cor);
-  
- // console.log (circuloX. leigth);
-  for(let contador in circuloX){
-  //console.log(contador);
-  circle(circuloX[contador ], circuloY[contador], 50);
-  circuloX[contador] += random(0, 3);
-  circuloY[contador] += random(-3, 3);
-    
-  if(circuloX[contador]>= width){
-    circuloX[contador]=0;
-    circuloY[contador] =random(height);
-  }
- 
-  }
-  
- 
-
- 
-  
-
-  
-  
-  
-  
+  stroke("blue");
+  fill("red");
+  //console.log(mouseIsPressed);
   if (mouseIsPressed) {
-    cor = color(random(0, 255), random(0, 255), random(0, 255), random(0, 100));
+    rect(mouseX, mouseY, 20, 35);
   }
 }
